732a08677e2ec78b34b1a988a7d9d9c461ea5e4e
Fernando Rojas <frojasq@yahoo.com>
2024-09-04 14:33:19 -0500

    Feat Ubicanos: en proceso

--- app/(routes)/(nosotros)/ubicanos/page.tsx
+++ app/(routes)/(nosotros)/ubicanos/page.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import { TitleSection } from "@/components/TitleSection";
+import { TabRedAutomotriz } from "./components/TabRedAutomotriz";
 
 export default function UbicanosPage() {
-  return <div>UbicanosPage</div>;
+  return <div className="p-6 border-2 border-black">
+    <TitleSection title="RED AUTOMOTRIZ DE AUTOMOTORES INKA"/>
+    <TabRedAutomotriz/>
+  </div>;
 }
